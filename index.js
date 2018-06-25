cb40f0d14e4d7d19e68951b9968ff3adc193e4dc
Shawn Salat <shawn.salat@gmail.com>
2018-06-25 11:57:16 -0700

    use mlab for mongodb and remove mocks

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { GraphQLServer } from 'graphql-yoga';
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://hackbox:hackbox2018@ds117701.mlab.com:17701/hackbox');
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
 
-const server = new GraphQLServer({ typeDefs, resolvers, mocks: true });
+const server = new GraphQLServer({ typeDefs, resolvers });
 mongoose.connection.once('open', function() {
   server.start(() => console.log('Server is running on localhost:4000'));
 });
