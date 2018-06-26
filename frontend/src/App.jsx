6f172b9f94578117232d06ecfce2d9a5c22a79d4
mwojick <35327565+mwojick@users.noreply.github.com>
2018-06-26 09:39:24 -0700

    remove App.test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          
+        </div>
       </div>
     );
   }
