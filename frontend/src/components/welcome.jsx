586f65f53263e1e1e16f2b55b7f1bc6f465da9ef
mwojick <35327565+mwojick@users.noreply.github.com>
2018-06-28 23:36:10 -0700

    h

--- frontend/src/components/welcome.jsx
+++ frontend/src/components/welcome.jsx
@@ -40,8 +40,8 @@ class Welcome extends Component {
   }
 
   componentDidMount() {
-    this.subscribeToNewRooms();
-    this.subscribeToRemoveRooms();
+    // this.subscribeToNewRooms();
+    // this.subscribeToRemoveRooms();
 
   }
 
