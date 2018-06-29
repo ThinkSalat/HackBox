33e4167ce595aa25b752dfb7d5b796ecc41ccb82
mwojick <35327565+mwojick@users.noreply.github.com>
2018-06-28 22:40:31 -0700

    serve static files to server in production

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
+const express = require("express");
 import mongoose from 'mongoose';
 mongoose.connect('mongodb://hackbox:hackbox2018@ds117701.mlab.com:17701/hackbox');
 
@@ -13,6 +14,11 @@ const options = {
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
+
+if (process.env.NODE_ENV === 'production') {
+  server.express.use(express.static("frontend/build"))
+}
+
 mongoose.connection.once('open', function() {
   server.start(options,() => console.log(`Server is running on localhost:${options.port}`));
 });
