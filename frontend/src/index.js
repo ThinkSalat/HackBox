b19f79d2592823350bc2dcfd6a7b9d169c67b96e
mwojick <35327565+mwojick@users.noreply.github.com>
2018-06-28 22:51:23 -0700

    set production to true

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,7 @@ import { split } from 'apollo-link';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
 
-let production = false;
+let production = true;
 
 let httpUri = "http://localhost:4000/graphql";
 let wsUri = "ws://localhost:4000/subscriptions";
