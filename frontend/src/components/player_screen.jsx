c11c349e151bc33e1b4c975252ae42bfcc93be71
mwojick <35327565+mwojick@users.noreply.github.com>
2018-07-01 23:06:17 -0700

    update plyr scren

--- frontend/src/components/player_screen.jsx
+++ frontend/src/components/player_screen.jsx
@@ -121,12 +121,6 @@ class PlayerScreen extends React.Component {
       return null;
     }
 
-    // let cards = responses.map((res) => {
-    //   return res.prompt
-    // });
-    
-    // debugger;
-
     let { 
       currentRound, 
       timer, 
@@ -134,16 +128,16 @@ class PlayerScreen extends React.Component {
 
     this.resIds = responses.map(res => res.id)
 
-    responses = responses.map(res => {
+    let cards = responses.map(res => {
       return <li key={res.id}>{res.prompt.prompt}</li>
     });
-    responses = <ul className='prompt-list'>{responses}</ul>;
+    cards = <ul className='prompt-list'>{cards}</ul>;
 
     return (
       <div>
         <h3>Current Round: {currentRound} / {this.room.numRounds} </h3>
         <h3>Timer: {timer}s</h3>
-        {this.answer(responses)}
+        {this.answer(cards)}
       </div>
     );
   }
